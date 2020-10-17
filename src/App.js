39281559bf7b871bb767042dfed109aeccf7e76c
kirill <jqweryjson@gmail.com>
2020-10-17 22:16:45 +0300

    add styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BryntumScheduler } from 'bryntum-react-shared';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BryntumScheduler
+        columns={[
+          {
+              field    : 'important',
+              text     : 'Important<div class="small-text">(React editor)</div>',
+              align    : 'center',
+              width    : 120,
+          },
+        ]}
+      />
     </div>
   );
 }
