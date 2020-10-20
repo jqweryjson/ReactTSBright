300c8ccf0376678f1f2edf333fc5229db7640e67
kirill <jqweryjson@gmail.com>
2020-10-20 22:05:33 +0300

    Updates

---
+++ precache-manifest.b11e512157cb5f8eaafbed9cea8fc459.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb8610c59ee2b9dca522f7672cbf1422",
+    "url": "/ReactTSBright/index.html"
+  },
+  {
+    "revision": "1658fae36162e09b4835",
+    "url": "/ReactTSBright/static/css/2.e688bb3a.chunk.css"
+  },
+  {
+    "revision": "7be7f4187bbb1cc5ad5c",
+    "url": "/ReactTSBright/static/css/main.a28039cb.chunk.css"
+  },
+  {
+    "revision": "1658fae36162e09b4835",
+    "url": "/ReactTSBright/static/js/2.b6b85b4f.chunk.js"
+  },
+  {
+    "revision": "1339ce7312b37887a6b55d6f40327865",
+    "url": "/ReactTSBright/static/js/2.b6b85b4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7be7f4187bbb1cc5ad5c",
+    "url": "/ReactTSBright/static/js/main.f6f6677e.chunk.js"
+  },
+  {
+    "revision": "345fe0bcb311a171524e",
+    "url": "/ReactTSBright/static/js/runtime-main.919c94b3.js"
+  },
+  {
+    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
+    "url": "/ReactTSBright/static/media/Roboto-Bold.b52fac2b.woff2"
+  },
+  {
+    "revision": "eb43b4c3b3b6cac224f76c0a524946a1",
+    "url": "/ReactTSBright/static/media/Roboto-Bold.eb43b4c3.woff"
+  },
+  {
+    "revision": "c8405cfa0df9fb2e47ef1c516cef59a8",
+    "url": "/ReactTSBright/static/media/Roboto-Light.c8405cfa.woff"
+  },
+  {
+    "revision": "d26871e8149b5759f814fd3c7a4f784b",
+    "url": "/ReactTSBright/static/media/Roboto-Light.d26871e8.woff2"
+  },
+  {
+    "revision": "7b225d4d9324ee54ce6d9ec50e339668",
+    "url": "/ReactTSBright/static/media/Roboto-Medium.7b225d4d.woff"
+  },
+  {
+    "revision": "90d1676003d9c28c04994c18bfd8b558",
+    "url": "/ReactTSBright/static/media/Roboto-Medium.90d16760.woff2"
+  },
+  {
+    "revision": "73f0a88bbca1bec19fb1303c689d04c6",
+    "url": "/ReactTSBright/static/media/Roboto-Regular.73f0a88b.woff2"
+  },
+  {
+    "revision": "878ad7158f6fa9136d796d4bad613bc2",
+    "url": "/ReactTSBright/static/media/Roboto-Regular.878ad715.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/ReactTSBright/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/ReactTSBright/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/ReactTSBright/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/ReactTSBright/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/ReactTSBright/static/media/fa-solid-900.ec763292.svg"
+  }
+]);
